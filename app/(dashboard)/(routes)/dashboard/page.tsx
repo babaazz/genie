22031d1e9538b175e9a43bd68b193323e5f391e7
Sunil Chauhan <sunil121ml@gmail.com>
2023-11-03 15:34:14 +0530

    Finishing Folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+const buttonProps = {
+  destructive: "destructive",
+};
+export default function DashboardPage() {
+  return <p>DashBoard Page(Protected)</p>;
+}
