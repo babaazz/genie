6d3d90e0276afbd830ebdfd981f410d4fd070692
Sunil Chauhan <sunil121ml@gmail.com>
2023-11-26 16:37:37 +0530

    Finishing up the image generation

---
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -0,0 +1,52 @@
+import * as z from "zod";
+
+export type optionType = {
+  value: string;
+  label: string;
+};
+
+export const formSchema = z.object({
+  prompt: z.string().min(1, {
+    message: "Image promot is required",
+  }),
+  amount: z.string().min(1),
+  resolution: z.string().min(1),
+});
+
+export const amountOptions: optionType[] = [
+  {
+    value: "1",
+    label: "1 Photo",
+  },
+  {
+    value: "2",
+    label: "2 Photo",
+  },
+  {
+    value: "3",
+    label: "3 Photo",
+  },
+  {
+    value: "4",
+    label: "4 Photo",
+  },
+  {
+    value: "5",
+    label: "5 Photo",
+  },
+];
+
+export const resolutionOptions: optionType[] = [
+  {
+    value: "256x256",
+    label: "256x256",
+  },
+  {
+    value: "512x512",
+    label: "512x512",
+  },
+  {
+    value: "1024x1024",
+    label: "1024x1024",
+  },
+];
